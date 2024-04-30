a39b2de95f9ccc81404f2c0f21f0b459a5760ad7
deling-google <99381034+deling-google@users.noreply.github.com>
2024-04-29 22:42:35 -0700

    Pick file and serve over http
    Add the ability to serve a file picked by the user over the http server.

--- hello_hotspot/app/src/main/java/com/google/hello_hotspot/FileServer.java
+++ hello_hotspot/app/src/main/java/com/google/hello_hotspot/FileServer.java
@@ -1,62 +1,177 @@
 package com.google.hello_hotspot;
 
-import android.annotation.SuppressLint;
-
+import android.content.ContentResolver;
+import android.net.Uri;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
 
 public class FileServer extends Thread {
-  private final int port;
+  static class Pair<T1, T2> {
+    public final T1 first;
+    public final T2 second;
 
-  public FileServer(int port) {
+    public Pair(T1 first, T2 second) {
+      this.first = first;
+      this.second = second;
+    }
+  }
+
+  /** HTTP response status line and headers, excluding body. */
+  static class Response {
+    private int statusCode;
+    private final List<Pair<String, String>> headers = new ArrayList<>();
+
+    private String statusText(int code) {
+      switch (code) {
+        case 200:
+          return "OK";
+        case 400:
+          return "Not Found";
+        default:
+          return "";
+      }
+    }
+
+    public void setStatusCode(int code) {
+      this.statusCode = code;
+    }
+
+    public void addHeader(String name, String value) {
+      headers.add(new Pair<>(name, value));
+    }
+
+    public byte[] getBytes() {
+      String newline = "\r\n";
+      StringBuilder sb = new StringBuilder();
+      sb.append(
+          String.format(Locale.getDefault(), "HTTP/1.0 %d %s", statusCode, statusText(statusCode)));
+      sb.append(newline);
+      for (Pair<String, String> header : headers) {
+        sb.append(header.first);
+        sb.append(": ");
+        sb.append(header.second);
+        sb.append(newline);
+      }
+      sb.append(newline);
+
+      return sb.toString().getBytes(StandardCharsets.UTF_8);
+    }
+  }
+
+  private final int port;
+  private final ContentResolver contentResolver;
+  private List<Uri> filesServed;
+
+  public FileServer(int port, ContentResolver contentResolver) {
     this.port = port;
+    this.contentResolver = contentResolver;
+  }
+
+  public void serveFiles(List<Uri> files) {
+    filesServed = files;
+  }
+
+  private byte[] getFileContent(Uri uri) {
+    System.out.println(uri);
+
+    if (uri == null) {
+      return null;
+    }
+
+    byte[] bytes = null;
+    try {
+      InputStream inputStream = contentResolver.openInputStream(uri);
+      bytes = inputStream.readAllBytes();
+      inputStream.close();
+    } catch (IOException e) {
+      System.out.println(Arrays.toString(e.getStackTrace()));
+    }
+    System.out.println(bytes.length);
+    return bytes;
+  }
+
+  private String getFileType(Uri uri) {
+    return uri == null ? null : contentResolver.getType(uri);
   }
 
   @Override
   public void run() {
     try {
-      InetAddress address = InetAddress.getByName("localhost");
-      System.out.println(address.toString());
-      ServerSocket serverSocket = new ServerSocket(port, 100, address);
+      ServerSocket serverSocket = new ServerSocket(port, 100, null);
       while (true) {
         Socket socket = serverSocket.accept();
         DataInputStream inputStream = new DataInputStream(socket.getInputStream());
         DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
 
+        String path = null;
         String request;
         do {
           request = inputStream.readLine();
-          System.out.println(request);
+          String prefix = "GET /";
+          if (request.startsWith(prefix)) {
+            path = request.substring(prefix.length());
+            int space = path.indexOf(' ');
+            if (space != -1) {
+              // "GET /path HTTP/1.1"
+              path = path.substring(0, space);
+            }
+          }
         } while (request.length() != 0);
 
-        String newline = "\r\n";
-        String status = "HTTP/1.0 200 OK";
-        String body = "Here's your file!";
-        int length = body.getBytes(StandardCharsets.UTF_8).length;
-        @SuppressLint("DefaultLocale") String headers =
-            String.format(
-                "Content-Length: %d"
-                    + newline
-                    + "Content-type: text/plain; charset=utf-8"
-                    + newline
-                    + "Connection: Close",
-                length);
-        byte[] bytes =
-            (status + newline + headers + newline + newline + body + newline + newline)
-                .getBytes(StandardCharsets.UTF_8);
+        if (path != null) {
+          // GET method
+          int index;
+          try {
+            index=Integer.parseInt(path);
+          } catch (NumberFormatException e) {
+            index = -1;
+          }
 
-        try {
-          outputStream.write(bytes);
-          outputStream.flush();
+          System.out.println(path);
+          System.out.println(index);
+
+          Uri uri = (index >= 0 && index < filesServed.size()) ? filesServed.get(index) : null;
+          byte[] body = getFileContent(uri);
+          String type = getFileType(uri);
+
+          System.out.println(type);
+          Response response = new Response();
+
+          if (body != null && type != null) {
+            response.setStatusCode(200);
+            response.addHeader("Content-Length", Integer.toString(body.length));
+            response.addHeader("Content-Type", type);
+            response.addHeader("Connection", "Close");
+          } else {
+            response.setStatusCode(404);
+            response.addHeader("Connection", "Close");
+          }
+
+          try {
+            outputStream.write(response.getBytes());
+            if (body != null) {
+              outputStream.write(body);
+            }
+
+            outputStream.flush();
+            outputStream.close();
+            socket.close();
+          } catch (Exception e) {
+            System.out.println(Arrays.toString(e.getStackTrace()));
+          }
+        } else {
           outputStream.close();
           socket.close();
-        } catch (Exception e) {
-          System.out.println(Arrays.toString(e.getStackTrace()));
         }
       }
     } catch (Exception e) {
--- hello_hotspot/app/src/main/java/com/google/hello_hotspot/MainActivity.java
+++ hello_hotspot/app/src/main/java/com/google/hello_hotspot/MainActivity.java
@@ -1,17 +1,46 @@
 package com.google.hello_hotspot;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.PickVisualMediaRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+  ActivityResultLauncher<PickVisualMediaRequest> picker;
+  FileServer fileServer;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    FileServer fileServer = new FileServer(5000);
+    picker =
+        registerForActivityResult(
+            new ActivityResultContracts.PickMultipleVisualMedia(), this::onMediaPicked);
+
+    fileServer = new FileServer(50000, getContentResolver());
     fileServer.start();
+  }
+
+  public void pickMedia(View view) {
+    assert picker != null;
+    picker.launch(
+            new PickVisualMediaRequest.Builder()
+                    .setMediaType(ActivityResultContracts.PickVisualMedia.ImageAndVideo.INSTANCE)
+                    .build());
+  }
+
+  private void onMediaPicked(List<Uri> uris) {
+    if (uris.size() == 0) {
+      return;
+    }
+    fileServer.serveFiles(uris);
   }
 }
